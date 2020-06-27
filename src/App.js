2f17f2b69b2f5e614d92b1cf7daf9be4d6099509
NaveedSahotra3 <56886967+NaveedSahotra3@users.noreply.github.com>
2020-06-27 02:59:44 -0700

    no cha ge
    alaaa

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Component/header/Header'
+// import Form from './Component/form/form'
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div><Header /></div>
+   
+    </div>
+  );
+}
+
+export default App;
